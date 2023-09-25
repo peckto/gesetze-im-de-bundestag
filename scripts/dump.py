a490cdc859d13250141a576f37b9999546780f8b
Tobias Specht <specht.tobias@gmx.de>
2023-09-25 20:59:03 +0200

    Download vorgangsposition

--- scripts/dump.py
+++ scripts/dump.py
@@ -3,12 +3,10 @@ import json
 import requests
 
 
-def dump_vorgang():
-    url = 'https://search.dip.bundestag.de/api/v1/vorgang'
-    args = {'f.wahlperiode': '20', 'format': 'json', 'apikey': 'rgsaY4U.oZRQKUHdJhF9qguHMkwCGIoLaqEcaHjYLF'}
+def dump(url: str, args: dict[str, str]):
     last_cursor = None
     cursor = 0
-    vorgang = []
+    data = []
     while last_cursor != cursor:
         if cursor:
             args['cursor'] = cursor
@@ -20,10 +18,25 @@ def dump_vorgang():
         print(j['numFound'], j['cursor'])
         last_cursor = cursor
         cursor = j['cursor']
-        vorgang += j['documents']
+        data += j['documents']
+    
+    return data
 
+
+def dump_vorgang():
+    url = 'https://search.dip.bundestag.de/api/v1/vorgang'
+    args = {'f.wahlperiode': '20', 'format': 'json', 'apikey': 'rgsaY4U.oZRQKUHdJhF9qguHMkwCGIoLaqEcaHjYLF'}
+    vorgang = dump(url, args)
     json.dump(vorgang, open('vorgang.json', 'wt'), indent=4)
+
+
+def dump_vorgangsposition():
+    url = 'https://search.dip.bundestag.de/api/v1/vorgangsposition'
+    args = {'f.wahlperiode': '20', 'format': 'json', 'apikey': 'rgsaY4U.oZRQKUHdJhF9qguHMkwCGIoLaqEcaHjYLF'}
+    vorgang = dump(url, args)
+    json.dump(vorgang, open('vorgangsposition.json', 'wt'), indent=4)
 
 
 if __name__ == '__main__':
     dump_vorgang()
+    dump_vorgangsposition()
