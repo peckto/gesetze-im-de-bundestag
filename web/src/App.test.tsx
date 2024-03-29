f4e5ae0d71f8955083f5c43a06f92b13312f7da4
Tobias Specht <specht.tobias@gmx.de>
2024-03-29 08:56:26 +0100

    Add dummy unit test

---
+++ web/src/App.test.tsx
@@ -0,0 +1,10 @@
+//import * as React from 'react';
+import { describe, test } from 'vitest';
+//import { screen, render } from '@testing-library/react';
+//import App from './App';
+
+describe('unit: App', () => {
+  test('dummy', async () => {
+    return
+  });
+});
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import reactRefresh from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  return {
+    plugins: [
+      reactRefresh(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: 'src/setupTests.js',
+    },
+  };
 })
