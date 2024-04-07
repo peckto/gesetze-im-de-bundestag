226a7078f8af96380723606f184431106a9963bd
Tobias Specht <specht.tobias@gmx.de>
2024-04-07 09:45:05 +0200

    Add build time stamp

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => {
     plugins: [
       reactRefresh(),
     ],
+    define: {
+      __BUILD_DATE__: new Date(),
+    },
     test: {
       globals: true,
       environment: 'jsdom',
